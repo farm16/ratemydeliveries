a280219ecbe65dedf6eed12dd464bddba891afd0
Christopher Fajardo <christopher.fajardo73@gmail.com>
2020-11-30 14:38:50 -0500

    almost ready to prod fixed empty pages files

--- pages/underMaintenance.js
+++ pages/underMaintenance.js
@@ -3,9 +3,9 @@ const { Fragment } = require("react");
 import { NavBar } from "../components/layout/NavBar";
 export default function UnderMaintenance() {
   return (
-    <>
+    <div>
       <NavBar />
       <div>under Maintenance</div>;
-    </>
+    </div>
   );
 }
