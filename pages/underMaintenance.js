9b4efdc3c34db0a785efcaabb77e5e0f6f9c1adf
Christopher Fajardo <christopher.fajardo73@gmail.com>
2020-11-30 14:27:53 -0500

    almost ready to prod

--- pages/underMaintenance.js
+++ pages/underMaintenance.js
@@ -1,12 +1,11 @@
 const { Fragment } = require("react");
 
-import { Fragment } from "react";
 import { NavBar } from "../components/layout/NavBar";
 export const underMaintenance = () => {
   return (
-    <Fragment>
+    <>
       <NavBar />
       <div>under Maintenance</div>;
-    </Fragment>
+    </>
   );
 };
